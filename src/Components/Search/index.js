58d36ea4a781579fecd71fc929be878c1dfa42b0
Sam Chillcott <samchillcott@gmail.com>
2020-12-27 09:51:21 +0000

    worker saga function

--- src/Components/Search/index.js
+++ src/Components/Search/index.js
@@ -5,10 +5,7 @@ import axios from 'axios';
 const Search = () => {
     
     async function onInputChange(e) {
-        const api_key = "A3AbxmhldfvQlsrkuBCNzWP2PVmZhbMk"
-        const url = `http://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${e.target.value}?limit=20`;
-        const data = await axios.get(url);
-        console.log(data.data.data);
+       
     };
 
     return (
--- src/Components/redux/SearchSaga.js
+++ src/Components/redux/SearchSaga.js
@@ -1,10 +1,18 @@
 import types from '../Search/types';
 import { takeLatest, put, call } from 'redux-saga/effects';
+import axios from 'axios';
 
-function* setSearch() {
-
+export function* setSearch() {
+    try{
+        const api_key = "A3AbxmhldfvQlsrkuBCNzWP2PVmZhbMk"
+        const url = `http://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${e.target.value}?limit=20`;
+        const data = yield axios.get(url);
+        console.log(data.data.data);
+    } catch(e) {
+        console.log(e);
+    }
 }
 
-function* watchSearch() {
-    yield takeLatest(types.watchGetSearchResults)
+export function* watchSearch() {
+    yield takeLatest(types.watchGetSearchResults, setSearch)
 }
