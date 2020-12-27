6ed9fa6433a7e649c52e278604945465568469a4
Sam Chillcott <samchillcott@gmail.com>
2020-12-27 10:06:04 +0000

    connect store

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import Search from './Components/Search/index'
+import Search from './Components/Search/index';
+import { Provider } from 'react-redux';
+import store from './Store/createStore';
 
 function App() {
   return (
+    <Provider store={store} >
     <div>
       <Search /> 
     </div>
+    </Provider >
   );
 }
 
